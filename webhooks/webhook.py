628074d62087d1b87df02df9a4e3b536ef31b1f1
Sergii Ripa <sergey.ripa@jivygroup.com>
2018-06-01 14:41:41 +0300

    implement log get/delete/create

--- webhooks/webhook.py
+++ webhooks/webhook.py
@@ -5,34 +5,26 @@ from urlparse import urlparse
 app = Flask(__name__)
 credFile = str('/tmp/creds.json')
 
+
+API_KEY = "4d52b63c-f73a-4c27-8104-a3142512c517"
+LOGSET_ID = "c5838fd4-1d3d-4104-8293-87b72ddc5ef4"
+GET_LOGSET_URL = "https://rest.logentries.com/management/logsets/%s"
+LOG_URL = "https://rest.logentries.com/management/logs"
+LOG_NAME_PATTERN = "exchange:%s"
+
 @app.route('/payload', methods=['GET', 'POST'])
 def webhookServer():
-
-    # Define some vars
-    EVENT = None
-    GHE_URL = None
-    GHE_HOST = None
-    TOKEN = None
-
-    # Let's go ahead and make GET requests happy. You could use it to test firewall rules
     if request.method == 'GET':
         return jsonify({'method':'GET','status':'success'}), 200
-
     # Webhooks will make POST requests
     elif request.method == 'POST':
-
-        print ' '
-
         EVENT = request.headers.get('X-GitHub-Event')
         print '----------> Received event: ' + EVENT
         debugPrintWebhookJSON(request.json)
-
         if EVENT == "ping":
             return handle_ping()
-
         if EVENT == "push":
             return handle_push()
-
         if EVENT == "delete":
             return handle_delete()
 
@@ -40,16 +32,51 @@ def handle_ping():
     return jsonify({'event':'ping','status':'success'}), 200
 
 def handle_push():
-    print 'Event Action: ' + request.json["action"]
-    return jsonify({'event':'push','status':'success'}), 200
+    new_log_created = False
+    name = branch_name()
+    if get_log(name) is None:
+        # create Log
+        create_log(LOG_NAME_PATTERN.format(name))
+        new_log_created = True
+    return jsonify({'event':'push','status':'success', 'log_created' : new_log_created}), 200
 
 def handle_delete():
-    print 'Event Action: ' + request.json["action"]
-    return jsonify({'event':'delete','status':'success'}), 200
+    deleted = False
+    name = branch_name()
+    if not (get_log(name) is None):
+        # create Log
+        delete_log(LOG_NAME_PATTERN.format(name))
+        deleted = True
+    return jsonify({'event':'push','status':'success', 'log_deleted' : deleted}), 200
+
+def get_log(branch_name):
+    headers = {'x-api-key': API_KEY}
+    url = GET_LOGSET_URL.format(LOGSET_ID)
+    resp = requests.get(url, headers=headers).json()
+    for log in resp['logset']['logs_info']:
+        if branch_name in log['name']:
+                print "Found log for branch %s: %s".format(branch_name, log)
+                return log
+    print "No log found for branch %s".format(branch_name)
+    return None
+
+def create_log(name):
+    headers = {'x-api-key': API_KEY}
+    body = {"log":{"name": name,"source_type":"token", "logsets_info":[{"id": LOGSET_ID}]}}
+    r = requests.post(LOG_URL, data=json.dumps(body, separators=(',', ':')), headers=headers)
+    print "Create log result:", r.status_code, r.content
+
+def delete_log(log_id):
+    headers = {'x-api-key': API_KEY}
+    r = requests.delete(LOG_URL + "/" + log_id, headers=headers)
+    print "Delete log result:", r.status_code, r.content
+
+def branch_name():
+    ref = request.json['ref']
+    print "Fetched branch name: %s".format(ref)
+    return ref
 
 def debugPrintWebhookJSON(data):
-
-    # Debug output
     print ' '
     print '======= DEBUG: BEGIN REQUEST JSON ======='
     print(json.dumps(data))
